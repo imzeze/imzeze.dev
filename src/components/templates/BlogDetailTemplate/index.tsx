4a7bf8d635f843aa4b17bee964e4fd3f77b6df5d
imzeze <jihyekim019@gmail.com>
2024-02-15 16:04:16 +0900

    docs: mdx 파일 동적 fetch 하기

--- src/components/templates/BlogDetailTemplate/index.tsx
+++ src/components/templates/BlogDetailTemplate/index.tsx
@@ -1,11 +1,12 @@
 import { MDXRemote } from 'next-mdx-remote';
 import { BlogDetailPageProps } from '@/pages/blog/[slug]';
+import { TAG } from '@/contants/tag';
 
 export default function BlogDetailTemplate({ source }: BlogDetailPageProps) {
     return (
         <article className="markdown-body">
             <div>
-                <MDXRemote {...source} />
+                <MDXRemote {...source} scope={TAG} />
             </div>
         </article>
     );
