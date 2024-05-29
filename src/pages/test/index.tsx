afec40af9e1b79f0cc143bc20457f17a6e3dc352
samg <samg@samgui-MacBookPro.local>
2024-05-29 14:20:30 +0900

    Feat: file share 테스트

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,14 +1,21 @@
 import { useEffect } from 'react';
 
-export default function Page() {
+export default async function Page() {
+    const response = await fetch(
+        'https://magicphoto.s3.ap-northeast-2.amazonaws.com/pictures/1000_1716768077_NGwUve/1000_1716768077_NGwUve.mp4',
+    );
+    const buffer = await response.arrayBuffer();
+    const file = new File([buffer], '1000_1716768077_NGwUve.mp4', {
+        type: 'video/mp4',
+    });
+    const files = [file];
+
     useEffect(() => {
         if (window && window.navigator) {
             const btn = document.getElementById('test');
             btn?.addEventListener('click', () => {
                 window.navigator.share({
-                    title: 'MDN',
-                    text: 'Learn web development on MDN!',
-                    url: 'https://developer.mozilla.org',
+                    files,
                 });
             });
         }
