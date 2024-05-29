d01f5cf074b655383f2e45c1560fa93bd2e35b00
samg <samg@samgui-MacBookPro.local>
2024-05-29 14:22:12 +0900

    fix: build config 수정

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
             {
                 source: '/',
-                destination: '/test',
+                destination: '/about',
                 permanent: true,
             },
         ];
